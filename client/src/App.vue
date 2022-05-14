6b6a50aaa4566eb2fb0ae62ee48087beb13335f4
Admin <buldan.556@gmail.com>
2022-05-14 12:36:49 +0400

    Initial Demo

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <Page1 msg="wawa"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Page1 from './components/PageTwo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Page1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
